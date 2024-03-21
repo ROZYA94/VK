38b4883f1bb78a5ae58248f3af7fcfdc2062abfe
ROZYA94 <158459795+ROZYA94@users.noreply.github.com>
2024-03-21 13:57:45 +0300

    Модуль 1 Домашнее задание 7

--- solution (5).py
+++ solution (5).py
@@ -1,11 +1,7 @@
-#coding: utf-8
-integ = int(input())
-flot = float(input())
-intg_pos = int(input())
-print(f"{integ:0=+10}")
-print(f"{flot:#>10.2f}")
-res_current = (f"{(intg_pos):0>16b}")
-res_current = str(res_current)
-result = str(res_current)
-result = (result[::-1])
-print('_'.join(result[i:i+4] for i in range(0, len(result), 4))[::-1])
+s = input()
+n = s.split()
+c1 = len(n)
+c2 = 0
+for _ in n:
+    c2 += len(_)
+print(c2/c1)
